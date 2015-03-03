b2217495c11a42d71e7d6966cfd6133df7844cd1
Komsan Nurak <n.komsan@gmail.com>
2015-03-03 16:54:03 +0700

    production

--- ci/application/config/autoload.php
+++ ci/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session', 'xmlrpc');
+$autoload['libraries'] = array('session', 'database', 'xmlrpc');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session', 'xmlrpc');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'file');
+$autoload['helper'] = array('url', 'file', 'form');
 
 
 /*
